8f03d10d0fcffbea96f3a2ec5f23dccf59ef38c2
MosfiqurRubel <rubel.airin@gmail.com>
2025-09-10 17:53:41 +0600

    (Add) - Implementation of the lucide icon library for react applications

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import { SquarePlay } from "lucide-react";
+
 function App() {
   return (
     <div className="flex flex-col items-center">
       <h1 className="text-xl p-4 m-4">Start Coding in React 19</h1>
-      <p className="bg-gray-300 p-2 rounded-sm text-3xl">
+      <p className="flex items-center bg-gray-300 p-2 rounded-sm text-3xl">
         <a
           href="https://www.youtube.com/watch?v=hiiGUjEkzbM&list=PLIJrr73KDmRw-T8bdJn3XxVMbH-zlooKb"
           target="_blank"
@@ -10,7 +12,9 @@ function App() {
         >
           Learn React 19 With Code
         </a>
-        <span>▶️</span>
+        <span className="shrink-0">
+          <SquarePlay />
+        </span>
       </p>
     </div>
   );
